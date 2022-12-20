e0d7d00b258bcefab56e5b5ba530958de0ca5b05
Tom Pollard <tpollard@flashpoint-intel.com>
2022-12-20 14:10:35 -0500

    day 19 WIP

--- day19/day19.py
+++ day19/day19.py
@@ -4,7 +4,7 @@
 #
 from typing import Sequence, Union, Optional, Any, Dict, List, Tuple
 from pathlib import Path
-from collections import defaultdict
+from collections import defaultdict, ChainMap
 from dataclasses import dataclass
 import math
 import re
@@ -52,30 +52,149 @@ GEODE_COST_RE = re.compile(r"Each geode robot costs (\d+) ore and (\d+) obsidian
 
 
 ORE, CLAY, OBSIDIAN, GEODE = "ore", "clay", "obsidian", "geode"
-
-@dataclass
-class Cost():
-    ore: int
-    clay: int = 0
-    obsidian: int = 0
-
+ROBOTS = (GEODE, OBSIDIAN, CLAY, ORE)
+MATERIALS = (ORE, CLAY, OBSIDIAN)
+ITEMS = (ORE, CLAY, OBSIDIAN, GEODE)
 
 class Blueprint():
-    def __init__(self, number: int, cost: Dict[str, Cost]):
+    def __init__(self, number: int, cost: Dict[str, Dict[str, int]]):
         self.cost = cost
         self.number = number
+
+    # @classmethod
+    # def from_text_v0(cls, text: str) -> "Blueprint":
+    #     print(f">>> {text}")
+    #     number = int(NUMBER_RE.match(text).group(1))
+    #     ore_cost = Cost(*[int(val) for val in ORE_COST_RE.search(text).groups()])
+    #     clay_cost = Cost(*[int(val) for val in CLAY_COST_RE.search(text).groups()])
+    #     obsidian_cost = Cost(*[int(val) for val in CLAY_COST_RE.search(text).groups()])
+    #     vals = [int(val) for val in GEODE_COST_RE.search(text).groups()]
+    #     geode_cost = Cost(vals[0], 0, vals[1])
+    #     return cls(number, {ORE: ore_cost, CLAY: clay_cost, OBSIDIAN: obsidian_cost, GEODE: geode_cost})
 
     @classmethod
     def from_text(cls, text: str) -> "Blueprint":
         print(f">>> {text}")
         number = int(NUMBER_RE.match(text).group(1))
-        ore_cost = Cost(*[int(val) for val in ORE_COST_RE.search(text).groups()])
-        clay_cost = Cost(*[int(val) for val in CLAY_COST_RE.search(text).groups()])
-        obsidian_cost = Cost(*[int(val) for val in CLAY_COST_RE.search(text).groups()])
+
+        vals = [int(val) for val in ORE_COST_RE.search(text).groups()]
+        ore_cost = {ORE: vals[0], CLAY: 0, OBSIDIAN: 0}
+
+        vals = [int(val) for val in CLAY_COST_RE.search(text).groups()]
+        clay_cost = {ORE: vals[0], CLAY: 0, OBSIDIAN: 0}
+
+        vals = [int(val) for val in OBSIDIAN_COST_RE.search(text).groups()]
+        obsidian_cost = {ORE: vals[0], CLAY: vals[1], OBSIDIAN: 0}
+
         vals = [int(val) for val in GEODE_COST_RE.search(text).groups()]
-        geode_cost = Cost(vals[0], 0, vals[1])
+        geode_cost = {ORE: vals[0], CLAY: 0, OBSIDIAN: vals[1]}
+
         return cls(number, {ORE: ore_cost, CLAY: clay_cost, OBSIDIAN: obsidian_cost, GEODE: geode_cost})
 
+def round_up(number) -> int:
+    return int(math.ceil(max(number, 0)))
+
+
+class Execution():
+    def __init__(self, blueprint):
+        self.blueprint = blueprint
+        self.resources: Dict[str, int] = defaultdict(int)
+        self.robots: Dict[str, int] = defaultdict(int)
+        self.robots[ORE] = 1
+        self.step = 0
+
+    def quality(self) -> int:
+        while self.step < 24:
+            self.single_step()
+        return self.resources[GEODE] * self.blueprint.number
+
+    def run(self, steps: int = 1):
+        for _ in range(steps):
+            self.single_step()
+
+    def single_step(self):
+        # aliases
+        cost = self.blueprint.cost
+        resources = self.resources
+        robots = self.robots
+
+        self.step += 1
+        print(f"== Minute {self.step} ==")
+
+        new_robot = None
+        next_robot = {}
+        blocker = ""
+        for robot in ROBOTS:
+            # do we have robots producing all of the material for this next robot?
+            if all([robots[item] for item, amount in cost[robot].items() if amount]):
+                steps, blocker = build_time(robot, robots, resources, cost)
+
+                if steps == 0:
+                    print(f"We can build a new {robot}-collecting robot now.")
+                else:
+                    print(f"We'll be able to build a {robot}-collecting robot in {steps} steps")
+                next_robot[robot] = steps
+
+                steps2, _ = build_time(robot, robots, resources, cost, add_robot=blocker)
+                if steps2 >= steps:
+                    blocker = ""
+
+                if steps == 0 and new_robot is None and robot == blocker:
+                    new_robot = robot
+                    new_robot_steps = steps
+
+            # how to decide when to build more clay robots and when to just wait...?
+
+            else:
+                print(f"We can't build {robot}-collecting robots yet")
+                next_robot[robot] = None
+
+        if new_robot and new_robot_steps == 0:
+            spend = {
+                item: cost[new_robot][item] for item in MATERIALS if cost[new_robot][item]
+            }
+            print(describe_spend(new_robot, spend))
+            for item, amount in spend.items():
+                resources[item] -= amount
+
+        # collect resources with existing robots
+        for item, count in robots.items():
+            if count:
+                resources[item] += count
+                print(f"{count} {item}-collecting robots collect {count} {item}; "
+                      f"You now have {resources[item]} {item}.")
+   
+        if new_robot and new_robot_steps == 0:
+            robots[new_robot] += 1
+            print(f"The new {new_robot}-collecting robot is ready; "
+                  f"You now have {robots[new_robot]} of them.")
+       
+
+def build_time(target_robot: str, robots, resources, cost, add_robot: Optional[str] = None) -> Tuple[int, str]:
+    if not add_robot:
+        build_robots = robots
+        build_resources = resources
+    else:
+        build_robots = {name: count if name != add_robot else count + 1 for name, count in robots.items()}
+        build_resources = {item: amount - cost[add_robot][item] for item, amount in build_resources.items()}
+    steps = 0
+    max_item_steps = 0
+    blocker = ""
+    for item, amount in cost[target_robot].items():
+        if build_robots[item]:
+            item_steps = round_up((cost[target_robot][item] - build_resources[item]) / build_robots[item])
+            if item_steps > steps:
+                steps = item_steps
+            if item_steps > max_item_steps:
+                max_item_steps = item_steps
+                blocker = item
+    return steps, blocker
+
+def describe_spend(robot, spend) -> str:
+    parts = ["Spend"]
+    parts.append(" and ".join([f"{count} {item}" for item, count in spend.items() if count]))
+    parts.append(f"to start building a {robot}-collecting robot.")
+    return " ".join(parts)
 
 
 def solve2(lines: Lines) -> int:
@@ -85,7 +204,9 @@ def solve2(lines: Lines) -> int:
 def solve(lines: Lines) -> int:
     """Solve the problem."""
     blueprints = [Blueprint.from_text(line) for line in lines]
-    return 0
+    engine = Execution(blueprints[0])
+    engine.run(24)
+    return engine.quality()
 
 
 # PART 1
